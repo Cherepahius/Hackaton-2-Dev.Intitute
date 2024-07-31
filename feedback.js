ee2897805c20c8d6f1635a64a130344d68489750
DiGureev <dianagureev@gmail.com>
2024-07-31 22:13:36 +0300

    feedback

---
+++ feedback.js
@@ -0,0 +1,7 @@
+// Very good project and nice idea! It helped me to relax a bit in the evening :) So thank you guys! 
+// I didn't immediately understand how to use it, so maybe you could add some hints. 
+// I like that you practised the db connection and even the transaction, which wasn't mandatory. Cool!
+
+// Only I wish you used Neon DB or created your tables from script. As I don't have access to your local db, I have to recreate your table somehow, which I know nothing about. But I love that you even thought about login/logout and didn't miss a chance to try it out. 
+
+// Also, it would be nice if you used .gitignore to ignore node_modules. Not only is it good practice, but I actually got an error when running the application because of the module conflict. So don't forget to always put it in the ignore file :) 
